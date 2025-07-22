a3e3b17b6a69e97f9d45861722403aee17abf90c
metashfiq01791 <metashfiq01791@gmail.com>
2025-07-21 21:03:43 -0700

    project-4 done

---
+++ project-4-comming-soon/script.js
@@ -0,0 +1,27 @@
+const launchDate = new Date("2025-12-31T00:00:00").getTime();
+
+const countDown = setInterval(() => {
+  const now = new Date();
+  const distance = launchDate - now;
+
+  // calculate the time
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  //   if reach that time
+  if (distance <= 0) {
+    document.getElementById("days").innerText = "00";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+  }
+}, 1000);
