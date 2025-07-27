4c90c39545872c9b3ec46da6c7dfd3317f5a6cb7
metashfiq01791 <metashfiq01791@gmail.com>
2025-07-27 00:08:29 -0700

    add darklight

--- project-7-Image-grid/script.js
+++ project-7-Image-grid/script.js
@@ -15,3 +15,14 @@ function twoColumn() {
 function fourColumn() {
   setColumnWidth("25%");
 }
+
+// dark light
+const icon = document.getElementById("icon");
+icon.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "darkLight/sun.png";
+  } else {
+    icon.src = "darkLight/moon.png";
+  }
+});
